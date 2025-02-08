f5d2c9f6b45e883ddb64946b80afcf5760f8368e
josephHelfenbein <josephhelfenbein@gmail.com>
2025-02-08 14:41:53 -0500

    remove template stuff

--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -29,45 +29,6 @@ export function GlobalNav() {
           </h3>
         </Link>
       </div>
-      <button
-        type="button"
-        className="group absolute right-0 top-0 flex h-14 items-center gap-x-2 px-4 lg:hidden"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="font-medium text-gray-100 group-hover:text-gray-400">
-          Menu
-        </div>
-        {isOpen ? (
-          <XMarkIcon className="block w-6 text-gray-400" />
-        ) : (
-          <Bars3Icon className="block w-6 text-gray-400" />
-        )}
-      </button>
-
-      <div
-        className={clsx('overflow-y-auto lg:static lg:block', {
-          'fixed inset-x-0 bottom-0 top-14 mt-px bg-black': isOpen,
-          hidden: !isOpen,
-        })}
-      >
-        <nav className="space-y-6 px-2 pb-24 pt-5">
-          {demos.map((section) => {
-            return (
-              <div key={section.name}>
-                <div className="mb-2 px-3 text-xs font-semibold uppercase tracking-wider text-gray-400/80">
-                  <div>{section.name}</div>
-                </div>
-
-                <div className="space-y-1">
-                  {section.items.map((item) => (
-                    <GlobalNavItem key={item.slug} item={item} close={close} />
-                  ))}
-                </div>
-              </div>
-            );
-          })}
-        </nav>
-      </div>
     </div>
   );
 }
